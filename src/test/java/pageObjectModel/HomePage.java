f4bb550d51ee41015f3466004a7c34dcde6694e9
Keerthana Mahesh <keerthanamahesh448@gmail.com>
2025-11-20 21:34:25 +0530

    Gift page update

--- src/test/java/pageObjectModel/HomePage.java
+++ src/test/java/pageObjectModel/HomePage.java
@@ -10,10 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage{
-	
-	
-	
-	
+		
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
--- src/test/java/testCases/TestCaseImplementation.java
+++ src/test/java/testCases/TestCaseImplementation.java
@@ -2,12 +2,16 @@ package testCases;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjectModel.CabPage;
+import pageObjectModel.GiftPage;
 import pageObjectModel.HomePage;
 import pageObjectModel.HotelPage;
 import testBase.BaseClass;
+import utilities.ExcelUtilities;
+
 
 public class TestCaseImplementation extends BaseClass{
 
@@ -31,9 +35,45 @@ public class TestCaseImplementation extends BaseClass{
 		cabs.filter();
 		cabs.lowestprice();
 	}
+	
+	
+	@DataProvider(name="GiftFormData")
+	public String [][] getData() throws IOException
+	{
+		String path=".\\utilities\\Project.xlsx";
+		ExcelUtilities xlutil=new ExcelUtilities(path);//creating an object for XLUtility
+		
+		int totalrows=xlutil.getRowCount("Sheet1");	
+		int totalcols=xlutil.getCellCount("Sheet1",1);
+				
+		String logindata[][]=new String[totalrows][totalcols];
+		for(int i=1;i<=totalrows;i++)  
+		{		
+			for(int j=0;j<totalcols;j++)  //i is rows j is column
+			{
+				logindata[i][j]= xlutil.getCellData("Sheet1",i, j);  
+			}
+		}
+	return logindata;
+	}
 
-	
-	
+	@Test(dependsOnMethods={"method2"})
+	public void implementGiftCard(String name,String mobileno,String email) throws InterruptedException{
+	GiftPage gp = new GiftPage(driver);
+	gp.hoverMenu(driver);
+	gp.giftcardOption();
+	gp.executeBcard(driver);
+
+	gp.detailsForm(driver);
+	gp.sendName(name);
+	gp.sendMobileNo(mobileno);
+	gp.sendEmail(email);
+
+	gp.clickButton();
+
+	gp.getDisplayedMsg();
+	}
+
 	@Test
     public void method4() throws InterruptedException {
         HotelPage hotelPage = new HotelPage(driver);
@@ -43,7 +83,6 @@ public class TestCaseImplementation extends BaseClass{
         hotelPage.clickGuestDropdown();
         hotelPage.increaseAdultsCount();
 
-	
 	}
 }
 
