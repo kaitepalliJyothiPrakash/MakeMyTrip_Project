75d85c653954cfbc2f0423c319d827b5bd4674bd
Kaitepalli Jyothi Prakash <jyothiprakashkaitepalli@gmail.com>
2025-11-22 10:25:44 +0530

    Final Commit

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -2,12 +2,16 @@ package testBase;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -26,26 +30,74 @@ public class BaseClass {
 		FileReader file=new FileReader("./src//test//resources//config.properties");
 		p=new Properties();
 		p.load(file);
-		switch(br.toLowerCase()) {
-		case "chrome" :
-			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
-			break;
+		
+		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+		{
+			DesiredCapabilities capabilities=new DesiredCapabilities();
 			
-		case "edge" :
-			driver=new EdgeDriver();
-			break;
+			//Selecting Operating System
+			if(os.equalsIgnoreCase("windows"))
+			{
+				capabilities.setPlatform(Platform.WIN11);
+			}
+			else if(os.equalsIgnoreCase("mac"))
+			{
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else 
+			{
+				System.out.println("No matching os");
+				return;
+			}
+			//Selecting browser
+			switch(br.toLowerCase())
+			{
+			case "chrome" :
+				capabilities.setBrowserName("chrome");
+				break;
+				
+			case "edge" :
+				capabilities.setBrowserName("MicrosoftEdge");
+				break;
+				
+			case "firefox" :
+				capabilities.setBrowserName("firefox");
+				break;
+				
+			default : 
+				System.out.println("Invalid browser name..."); 
+				return;
+			}
 			
-		default :
-			System.out.println("Invalid browser name....");
-			return;
+			URI uri = URI.create("http://10.232.46.40:4444/wd/hub");
+			driver=new RemoteWebDriver(uri.toURL(),capabilities);
+			
+		}
+		if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+		{
+			//Selecting Browser
+			switch(br.toLowerCase()) 
+			{
+			case "chrome" :
+				WebDriverManager.chromedriver().setup();
+				driver=new ChromeDriver();
+				break;
+				
+			case "edge" :
+				driver=new EdgeDriver();
+				break;
+				
+			default :
+				System.out.println("Invalid browser name....");
+				return;
+			}
 		}
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		driver.get(p.getProperty("appURL"));//Reading URL from properties file
 		driver.manage().window().maximize();
-		Thread.sleep(2000);
+		
 	}
 	
 	@AfterClass
--- src/test/java/testCases/TestCaseImplementation.java
+++ src/test/java/testCases/TestCaseImplementation.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import pageObjectModel.CabPage;
 import pageObjectModel.GiftPage;
 import pageObjectModel.HomePage;
@@ -18,6 +25,12 @@ public class TestCaseImplementation extends BaseClass{
 
 	
 	@Test(priority=1)
+	@Description("Closing Registraion popup on Home Page")
+	@Epic("EP001")
+	@Feature("Feature1: Closing popup")
+	@Story("Story: Closing popup")
+	@Step("Verifying the popup closed")
+	@Severity(SeverityLevel.BLOCKER)
 	public void closingPopupInHomePage()
 	{
 		HomePage homepage=new HomePage(driver);
@@ -40,6 +53,12 @@ public class TestCaseImplementation extends BaseClass{
 	}
 
 	@Test(dependsOnMethods= "closingPopupInHomePage",dataProvider="CabData")
+	@Description("Book one way outstation cab, From Delhi to Manali, himachal Pradesh, give future date & time & Car type should be SUV; Display the lowest charges")
+	@Epic("EP001")
+	@Feature("Feature2: Book one way outstation cab and Display the lowest charges")
+	@Story("Story: Book one way outstation cab ")
+	@Step("Verifying the cab charges and booking cab")
+	@Severity(SeverityLevel.CRITICAL)
 	public void implementCab(String pick,String dropLocation,String months,String date) throws InterruptedException, IOException
 	{
 		CabPage cabs=new CabPage(driver);
@@ -70,6 +89,12 @@ public class TestCaseImplementation extends BaseClass{
 	}
 
 	@Test(dependsOnMethods="implementCab",dataProvider = "GiftFormData")
+	@Description("Find Group Gifting in Gift Cards, fill card details & give invalid email; capture & display the error message")
+	@Epic("EP001")
+	@Feature("Feature3: Find Group Gifting in Gift Cards")
+	@Story("Story: capture & display the error message")
+	@Step("Verifying the email address")
+	@Severity(SeverityLevel.MINOR)
 	public void implementGiftCard(String name,String mobileno,String email) throws InterruptedException, IOException{
 	GiftPage gp = new GiftPage(driver);
 	gp.hoverMenu(driver);
@@ -93,6 +118,12 @@ public class TestCaseImplementation extends BaseClass{
 	}
 
 	@Test(dependsOnMethods= "implementGiftCard")
+	@Description("On the Hotel booking page, extract all the numbers for Adult persons and store in a List; Display the same")
+	@Epic("EP001")
+	@Feature("Feature4: Extract all the numbers for Adult persons and store in a List")
+	@Story("Story: The numbers for Adult persons and store in a List")
+	@Step("Verifying the numbers for Adult persons")
+	@Severity(SeverityLevel.NORMAL)
     public void hotelDetails() throws InterruptedException {
         HotelPage hotelPage = new HotelPage(driver);
         hotelPage.openHotelsPage();
